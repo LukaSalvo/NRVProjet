b33911d9fd0c9aea52126d10e98dca6ebc9887e8
LukaSalvo <luka.salvo23@gmail.com>
2024-11-07 13:34:19 +0100

    Ajout du setconfig et du getInstance et ajout du db.config.ini

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-use \iutnc\nrv\dispatch\Dispatcher;
+
+
+require_once "vendor/autoload.php";
+require_once 'src/classes/dispatch/Dispatcher.php';
+
+use iutnc\nrv\repository\NRVRepository;
+use iutnc\nrv\dispatch\Dispatcher;
 
 session_start();
 
+NRVRepository::setconfig('src/config/db.config.ini');
 
 $dispatcher = new Dispatcher();
 $dispatcher->run();
--- src/classes/repository/NRVRepository.php
+++ src/classes/repository/NRVRepository.php
@@ -5,6 +5,7 @@
 namespace iutnc\nrv\repository;
 
 use iutnc\deefy\repository\DeefyRepository;
+use iutnc\nrv\exception\InvalidPropertyNameException;
 
 class NRVRepository{
 
@@ -16,6 +17,34 @@ class NRVRepository{
         $this->pdo = new PDO($conf['dsn'], $conf['user'], $conf['pass'], [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
+    }
+
+
+    public static function getInstance():? NRVRepository {
+        if (is_null(self::$instance)) {
+            if (empty(self::$config)) {
+                throw new \Exception("NRV Repository not configured");
+
+            }
+            self::$instance = new NRVRepository(self::$config);
+        }
+        return self::$instance;
+    }
+
+    public static function setConfig(string $file) {
+        $conf = parse_ini_file($file);
+        if ($conf === false) {
+            throw new \Exception("Erreur pendant la lecture du fichier de configuration.");
+        }
+        if (!isset($conf['host'], $conf['dbname'], $conf['username'])) {
+            throw new \Exception("Le fichier de configuration ne contient pas toutes les clés nécessaires.");
+        }
+
+        self::$config = [
+            'dsn' => "mysql:host=" . $conf['host'] . ";dbname=" . $conf['dbname'] ,
+            'user' => $conf['username'],
+            'pass' => $conf['password']
+        ];
     }
 
     public function getUserByEmail(string $email): ?array {
