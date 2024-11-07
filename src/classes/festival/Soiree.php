adc8c335de621c8399f821eef335f57acdcc0814
Joey <joey.rosenkranz6@etu.univ-lorraine.fr>
2024-11-07 11:18:12 +0100

    Ajout de la classe soirée

--- src/classes/festival/Soiree.php
+++ src/classes/festival/Soiree.php
@@ -6,16 +6,17 @@ namespace iutnc\nrv\festival;
 
 use iutnc\nrv\exception as E;
 class Soiree{
+    private string $nom;
+    private string $date;
+    private array $ListeSpectacle;
+    private Lieu $lieu;
 
-    protected int $id_soiree;
-    protected $date;
 
-    protected Lieu $id_lieu;
-
-
-    public function __construct(int $id, $date){
+    public function __construct(string $nom, string $date, array $ListeSpectacle, Lieu $lieu){
         $this->id_soiree = $id;
         $this->date = $date;
+        $this->ListeSpectacle = $ListeSpectacle;
+        $this->lieu = $lieu;
 
     }
 
