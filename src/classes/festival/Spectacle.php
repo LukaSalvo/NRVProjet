90de7f66d28a76a7d7fe5af1c8e6c01feba268ba
Pacôme Terrier <pacome.terrier1@etu.univ-lorraine.fr>
2024-11-07 14:22:45 +0100

    chgmt nom de fichier

---
+++ src/classes/festival/Spectacle.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace iutnc\nrv\festival;
+
+use iutnc\nrv\exception as E;
+class Spectacle  {
+
+
+    protected string $nomSpectacle;
+
+    protected string $style;
+
+    protected int $duree;
+
+    public function __construct( string $nomSpectacle, string $style, int $duree){
+        $this->nomSpectacle = $nomSpectacle;
+        $this->style = $style;
+        $this->duree = $duree;
+    }
+
+
+    public function __get($attribut): mixed {
+        if(property_exists($this, $attribut)){
+            return $this->$attribut;
+        }else{
+            return throw new E\InvalidPropertyNameException("erreur ");
+        }
+    }
+
+    public function __set(string $attribut,mixed $value){
+        if (property_exists($this,$attribut)){
+            $this->$attribut = $value;
+        }
+
+        else throw new E\InvalidPropertyNameException("$attribut : invalid property");
+
+    }
+
+
+
+}
