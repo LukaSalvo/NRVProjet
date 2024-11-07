61e7992add41c145276308b5014bd7c228c06db9
LukaSalvo <luka.salvo23@gmail.com>
2024-11-07 14:37:58 +0100

    Ajout des insert dans la base de donnée

--- NRVimport.sql
+++ NRVimport.sql
@@ -73,3 +73,112 @@ CREATE TABLE `soiree2spectacle` (
 	FOREIGN KEY (`id_soiree`) REFERENCES   `soiree`(`id_soiree`),
 	FOREIGN KEY (`id_spectacle`) REFERENCES `spectacle`(`id_spectacle`)
 );
+
+
+INSERT INTO lieu (id_lieu ,nom_lieu, adresse, nb_place) VALUES
+                                                       (1,'Zénith de Nancy', 'Rue du Zénith, Maxéville', 5000),
+                                                       (2,'L\'Autre  Gérome Canals', '45 Bd d\'Austrasie, Nancy', 1200),
+                                                       (3,'Théâtre de la Manufacture', '10 Rue Baron Louis, Nancy', 850);
+
+
+
+INSERT INTO artiste (id_artiste , nom_artiste) VALUES
+                                      (1,'The Rocking Tigers'),
+                                      (2,'DestAcid'),
+                                      (3,'Techno Masters'),
+                                      (4,'Acid Crew'),
+                                      (5,'Electric Thunder'),
+                                      (6,'Metal Storm'),
+                                      (7,'Tristan Tornow'),
+                                      (8,'The Vibes'),
+                                      (9,'Neo Noir'),
+                                      (10,'Urban Funk'),
+                                      (11,'Hypno Sound'),
+                                      (12,'Strobe Vision'),
+                                      (13,'Night Ravers'),
+                                      (14,'Sonic Drive');
+
+
+
+
+INSERT INTO spectacle (id_spectacle, nomSpec, style, duree , id_soiree) VALUES
+                                                                            (1, 'Rock Vibes', 'Rock', 90, 1),
+                                                                            (2, 'Hardcore Thunder', 'Hard Rock', 105, 1),
+                                                                            (3, 'Techno Pulse', 'Techno', 100, 2),
+                                                                            (4, 'Acid Flow', 'Acid', 95, 2),
+                                                                            (5, 'Electric Night', 'Electronic', 85, 3),
+                                                                            (6, 'Metal Scream', 'Metal', 110, 3),
+                                                                            (7, 'Blue Fusion', 'Blues', 90, 4),
+                                                                            (8, 'Synthwave Dreams', 'Synthwave', 80, 4),
+                                                                            (9, 'Groove Jam', 'Funk', 75, 5),
+                                                                            (10, 'Dark Soundscapes', 'Darkwave', 90, 5),
+                                                                            (11, 'Hypnotic Journey', 'Trance', 95, 6),
+                                                                            (12, 'Neon Nights', 'Electro', 85, 6),
+                                                                            (13, 'Night Bassline', 'House', 90, 7),
+                                                                            (14, 'Pulse Driver', 'Drum and Bass', 105, 7);
+
+
+INSERT INTO `soiree` (id_soiree , nom_soiree,id_lieu, date) VALUES
+                                    (1, 'Rock Fest', 1, '2024-06-01'),
+                                      (2, 'Electro Vibes', 2, '2024-06-02'),
+                                      (3, 'Metal Mania', 3, '2024-06-03'),
+                                      (4, 'Jazz Night', 1, '2024-06-04'),
+                                      (5, 'Indie Sounds', 2, '2024-06-05'),
+                                      (6, 'Pop Parade', 3, '2024-06-06'),
+                                      (7, 'Acoustic Evening', 1, '2024-06-07'),
+                                      (8, 'Reggae Groove', 2, '2024-06-08'),
+                                      (9, 'Classical Harmonies', 3, '2024-06-09'),
+                                      (10, 'Folk Fest', 1, '2024-06-10'),
+                                      (11, 'Hip Hop Beats', 2, '2024-06-11'),
+                                      (12, 'Dancefloor Fever', 3, '2024-06-12');
+
+
+
+INSERT INTO `user` (nom_user, email, password, role) VALUES
+                                                         ('Festival Staff', 'staff@nrvfest.com', 'password123', 1),
+                                                         ('Festival Admin', 'admin@nrvfest.com', 'adminpassword', 100);
+
+
+
+
+INSERT INTO `soiree2spectacle` (id_spectacle, id_soiree) VALUES
+                                                             (1, 1), (2, 1),
+                                                             (3, 2), (4, 2),
+                                                             (5, 3), (6, 3),
+                                                             (7, 4), (8, 4),
+                                                             (9, 5), (10, 5),
+                                                             (11, 6), (12, 6),
+                                                             (13, 7), (14, 7),
+                                                             (1, 8), (3, 8),
+                                                             (4, 9), (5, 9),
+                                                             (6, 10), (7, 10),
+                                                             (8, 11), (9, 11),
+                                                             (10, 12), (11, 12);
+INSERT INTO `spectacle2artiste` (id_spectacle, id_artiste) VALUES
+                                                               (1, 1),
+                                                               (1, 2),
+                                                               (2, 3),
+                                                               (2, 4),
+                                                               (3, 5),
+                                                               (3, 6),
+                                                               (4, 7),
+                                                               (4, 8),
+                                                               (5, 9),
+                                                               (5, 10),
+                                                               (6, 11),
+                                                               (6, 12),
+                                                               (7, 13),
+                                                               (7, 14),
+                                                               (8, 1),
+                                                               (8, 3),
+                                                               (9, 4),
+                                                               (9, 5),
+                                                               (10, 6),
+                                                               (10, 7),
+                                                               (11, 8),
+                                                               (11, 9),
+                                                               (12, 10),
+                                                               (12, 11),
+                                                               (13, 12),
+                                                               (13, 13),
+                                                               (14, 14);
